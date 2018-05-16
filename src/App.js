c6a720204a144c5bb75654ef760251ba23294cf4
Keenan Thompson <keenan@keenant.com>
2018-05-15 21:28:00 -0500

    Add fonts, name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Keenan Thompson
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          My name is Keenan and I am currently a senior studying Computer Science at the University of Wisconsin — Madison.
+          I have experience in starting a company and working as a team to develop cool software, websites and more.
         </p>
       </div>
     );
